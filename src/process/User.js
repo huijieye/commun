eed0caff7910133058fa3c7c47d208edcf2070c7
huijie <huijie.ye@supdeweb-paris.com>
2018-04-24 19:27:58 +0200

    manque branchement de reducers

--- src/process/User.js
+++ src/process/User.js
@@ -1,7 +1,15 @@
 import {fetchLogin,loginSucces,loginFail,loginOut} from '../actions/User';
 import axios from 'axios';
 
-export function loginProcess(dispach,login) {
-    dispach(fetchLogin())
+export function loginProcess(dispatch,login) {
+    dispatch(fetchLogin())
+    axios.get('https://noodledistress.herokuapp.com/user/login/'+login).then(res => {
+        dispatch(loginSucces(res))
+    }).catch(error => {
+        dispatch(loginFail(error))
+    })
+}
 
+export function loginOutProcess(dispatch) {
+    dispatch(loginOut())
 }
